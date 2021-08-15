3b8f889b0051c57a81993282579b2c9f0f2d36e7
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 18:49:35 +0100

    Added delete_all() methods to console.py

--- school_start_point/console.py
+++ school_start_point/console.py
@@ -10,6 +10,9 @@ import repositories.student_repository as student_repository
 #once created add student_repository.delete_all()
 #then course_repository.delete_all()
 #then student_repository.delete_all()
+student_repository.delete_all()
+course_repository.delete_all()
+student_course_repository.delete_all()
 
 #create instances of Course, Student, and Student_Course classes
 #save these instances using a method from the corresponding repository
