cdecca4053d464ff8d5f8b313213ea1c502ab344
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 14:16:11 +0100

    Student_Course Class initialised

---
+++ school_start_point/models/student_course.py
@@ -0,0 +1,6 @@
+class Student_Course:
+    def __init__(self, student, course, grade, id=None):
+        self.student = student
+        self.course = course
+        self.course = course
+        self.id = id
