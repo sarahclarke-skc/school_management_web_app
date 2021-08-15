0279574a65730790bfb457cba475c31cefac4cdf
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 14:36:31 +0100

    student_repo set up, no methods

---
+++ school_start_point/repositories/student_repository.py
@@ -0,0 +1,17 @@
+from db.run_sql import run_sql
+from models.student import Student
+from models.course import Course
+
+#save
+
+#select all
+
+#select one according to id
+
+#delete all
+
+#delete one according to id
+
+#update student
+
+#find the courses a student is enrolled on
