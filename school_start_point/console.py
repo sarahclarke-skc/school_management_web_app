874ec6e46130faed947997853e4691969f5388cb
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 19:32:14 +0100

    Added .save() methods to console.py to populate the tables

--- school_start_point/console.py
+++ school_start_point/console.py
@@ -15,18 +15,28 @@ course_repository.delete_all()
 student_course_repository.delete_all()
 
 #create instances of Course, Student, and Student_Course classes
+
+#save these instances using a method from the corresponding repository
+
 student = Student("Anton", "Artiukov", "aartiukov@gmail.com", "+7911231234", "Upper Intermediate", True)
+student_repository.save(student)
 
 student_2 = Student("Liza", "Bogdanova", "liza_b@gmail.com", "+7911222333", "Intermediate", True)
+student_repository.save(student_2)
 
 course = Course("English File", "Upper Intermediate", "Tuesday Thursday", "19:30", 90, "36 weeks")
+course_repository.save(course)
 
 course_2 = Course("English File", "Intermediate", "Wednesday Friday", "19:15", 120, "30 weeks")
+course_repository.save(course_2)
 
 student_course = Student_Course(student, course, "A")
+student_course_repository.save(student_course)
 
 student_course_2 = Student_Course(student_2, course_2, "B")
-#save these instances using a method from the corresponding repository
+student_course_repository.save(student_course_2)
+
+
 
 
 pdb.set_trace()
