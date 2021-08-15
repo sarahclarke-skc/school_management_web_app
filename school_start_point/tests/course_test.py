acd27bcd7558a99d1546236b3960937cf433227e
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 16:48:38 +0100

    test course has name passing

--- school_start_point/tests/course_test.py
+++ school_start_point/tests/course_test.py
@@ -5,5 +5,5 @@ class TestCourse(unittest.TestCase):
     def setUp(self):
         self.course = Course("English File", "Upper Intermediate", "Tuesday Thursday", "19:30", 90, "36 weeks")
 
-    def test_course_has_description(self):
-        self.
+    def test_course_has_name(self):
+        self.assertEqual("English File", self.course.name)
