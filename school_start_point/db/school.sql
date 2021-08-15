381d5ed313666b9ec1823b366618e8c2795fb70a
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 14:08:13 +0100

    Created tables in school.sql

---
+++ school_start_point/db/school.sql
@@ -0,0 +1,30 @@
+DROP TABLE student_courses;
+DROP TABLE students;
+DROP TABLE courses;
+
+CREATE TABLE students(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    telephone VARCHAR(255),
+    level VARCHAR(255),
+    enrolled BOOLEAN
+);
+
+CREATE TABLE courses(
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(255),
+    level VARCHAR(255),
+    days VARCHAR(255),
+    start_time VARCHAR(255),
+    duration INT,
+    length_of_course VARCHAR(255)
+);
+
+CREATE TABLE student_courses(
+    id SERIAL PRIMARY KEY,
+    student_id INT REFERENCES students(id) ON DELETE CASCADE,
+    course_id INT REFERENCES courses(id) ON DELETE CASCADE,
+    grade VARCHAR(255)
+);
