65001713843280408c2b291e17c7c3239ff992cf
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 18:29:37 +0100

    Added select_all() to course_repo

--- school_start_point/controllers/course_controller.py
+++ school_start_point/controllers/course_controller.py
@@ -4,3 +4,7 @@ from models.course import Course
 import repositories.course_repository as course_repository
 
 courses_blueprint = Blueprint("courses", __name__)
+
+@courses_blueprint.route('/courses')
+def list_all_courses():
+    pass
--- school_start_point/repositories/course_repository.py
+++ school_start_point/repositories/course_repository.py
@@ -3,9 +3,23 @@ from db.run_sql import run_sql
 from models.course import Course
 from models.student import Student
 
-#save
-
+def save(course):
+    sql = "INSERT INTO courses(name, level, days, start_time, duration, length_of_course) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id"
+    values = [course.name, course.level, course.days, course.start_time, course.duration, course.length_of_course]
+    results = run_sql(sql, values)
+    return course
 #select all
+
+def select_all():
+    course =[]
+    sql = "SELECT * FROM courses"
+    results = run_sql
+
+    for row in results:
+        course = Course(results['name'], results['level'], results['days'], results['start_time'], results['duration'], results['length_of_course'], results['id'])
+        results.append(course)
+        
+    return course
 
 #select one according to id
 
