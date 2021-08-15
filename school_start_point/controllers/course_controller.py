d74372ab00f3f697e47660e84e19298f11ec9b4e
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 18:33:22 +0100

    list_all_courses route defined in course_controller

--- school_start_point/controllers/course_controller.py
+++ school_start_point/controllers/course_controller.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.course import Course
+from repositories import course_repository, student_course_repository
 import repositories.course_repository as course_repository
 
 courses_blueprint = Blueprint("courses", __name__)
 
 @courses_blueprint.route('/courses')
 def list_all_courses():
-    pass
+    courses = course_repository.select_all()
+    return render_template('courses/index.html')
