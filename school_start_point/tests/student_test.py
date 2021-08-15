81d69d793fd3bb98d179339a375f9c4e9838233d
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 16:59:04 +0100

    test student has first name passing

--- school_start_point/tests/student_test.py
+++ school_start_point/tests/student_test.py
@@ -4,3 +4,8 @@ from models.student import Student
 class TestStudent(unittest.TestCase):
     def setUp(self):
         self.student = Student("Anton", "Artiukov", "aartiukov@gmail.com", "+7911231234", "Upper Intermediate", True)
+    
+    def test_student_has_first_name(self):
+        self.assertEqual("Anton", self.student.first_name)
+
+    #write method to join first and last name
