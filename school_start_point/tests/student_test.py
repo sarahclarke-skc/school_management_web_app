f7945053936b400b98d7b4224fa63cf8af887492
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 16:59:58 +0100

    test student has last name passing

--- school_start_point/tests/student_test.py
+++ school_start_point/tests/student_test.py
@@ -7,5 +7,8 @@ class TestStudent(unittest.TestCase):
     
     def test_student_has_first_name(self):
         self.assertEqual("Anton", self.student.first_name)
+    
+    def test_student_has_last_name(self):
+        self.assertEqual("Artiukov", self.student.last_name)
 
     #write method to join first and last name
