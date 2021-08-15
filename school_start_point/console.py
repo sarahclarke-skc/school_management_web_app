6117870fa6e2fca2b3a7a35eeb94d1bffdaec7a7
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-15 18:59:36 +0100

    Added instances of Student, Course, and Student_Course Classes to console.py

--- school_start_point/console.py
+++ school_start_point/console.py
@@ -15,6 +15,17 @@ course_repository.delete_all()
 student_course_repository.delete_all()
 
 #create instances of Course, Student, and Student_Course classes
+student = Student("Anton", "Artiukov", "aartiukov@gmail.com", "+7911231234", "Upper Intermediate", True)
+
+student_2 = Student("Liza", "Bogdanova", "liza_b@gmail.com", "+7911222333", "Intermediate", True)
+
+course = Course("English File", "Upper Intermediate", "Tuesday Thursday", "19:30", 90, "36 weeks")
+
+course_2 = Course("English File", "Intermediate", "Wednesday Friday", "19:15", 120, "30 weeks")
+
+student_course = Student_Course(student, course, "A")
+
+student_course_2 = Student_Course(student_2, course_2, "B")
 #save these instances using a method from the corresponding repository
 
 
