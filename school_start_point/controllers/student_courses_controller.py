ce4284ae31a775dacf55d493fc4072d99d681c85
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-16 09:35:32 +0100

    Added /student_courses route

--- school_start_point/controllers/student_courses_controller.py
+++ school_start_point/controllers/student_courses_controller.py
@@ -6,3 +6,23 @@ import repositories.student_course_repository as student_course_repository
 import repositories.student_repository as student_repository
 
 student_courses_blueprint = Blueprint("student_courses", __name__)
+
+#Index --> should I use the route as student_course OR should I use 'enrolment'?
+@student_courses_blueprint.route("/student_course")
+def student_course():
+    student_courses = student_course_repository.select_all()
+    return render_template("student_courses/index.html")
+
+#New
+
+#Create
+
+#Edit
+
+#Update
+
+#Delete
+
+#Show
+
+#Add student to course?
