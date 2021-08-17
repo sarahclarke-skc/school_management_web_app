e0b6c1633b12c6e5a7de91153304200b1be32116
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-17 11:07:18 +0100

    Update check box for enrolled now working

--- school_start_point/models/student.py
+++ school_start_point/models/student.py
@@ -1,6 +1,6 @@
 class Student:
 
-    def __init__(self, first_name, last_name, email, telephone, level, enrolled, id=None):
+    def __init__(self, first_name, last_name, email, telephone, level, enrolled=False, id=None):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
